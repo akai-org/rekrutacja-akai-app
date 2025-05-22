6fa7a21573e313ff72feb67d7a03d67ba54744b3
Kamil <kamil.suwiczak@wp.pl>
2025-05-22 16:42:53 +0200

    added basic config for login

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+let routes = require('./login.js');
+routes(app);
+
+app.listen(port);
+console.log(`Server is running on port ${port}`);
